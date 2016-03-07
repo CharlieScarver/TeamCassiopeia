402820501c3ec2bc5c1b33fa0d7417cb3d6715af
CharlieScarver <s.apostolovd@gmail.com>
2016-03-07 04:14:19 +0200

    Editted table colliders; Added basic move script for the white ball

---
+++ BilliardsGame/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public int force = 15;
+    public Rigidbody body;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveHorizontal = Input.GetAxis("Horizontal");
+	    moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * force, 0.0f, moveVertical * force);
+        //body.velocity = movement * force;
+        body.AddForce(movement);
+
+    }
+}
